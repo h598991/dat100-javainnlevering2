d4242c43817ef5df4dc12f71312a0845c102f0fb
h161180 <Andersw70@gmail.com>
2021-09-21 17:50:15 +0200

    Update Tabeller.java

--- src/no/hvl/dat100/lab5/tabeller/Tabeller.java
+++ src/no/hvl/dat100/lab5/tabeller/Tabeller.java
@@ -2,9 +2,9 @@ package no.hvl.dat100.lab5.tabeller;
 
 public class Tabeller {
 	public static void main(String[] args) {
-		int[] table = {1,2,3,4};
-		int[] table2 = {5,6,7,8};
-		
+		int[] table = { 1, 2, 3, 4 };
+		int[] table2 = { 5, 6, 7, 8 };
+
 		System.out.println(tilStreng(table));
 		System.out.println(posisjonTall(table, 3));
 		System.out.println(tilStreng(settSammen(table, table2)));
@@ -14,8 +14,8 @@ public class Tabeller {
 
 	// a) Anders
 	public static void skrivUt(int[] tabell) {
-		
-		for (int i=0; i<4; i++) {
+
+		for (int i = 0; i < 4; i++) {
 			System.out.print(tabell[i] + ",");
 		}
 
@@ -23,42 +23,41 @@ public class Tabeller {
 
 	// b) Fredrik
 	public static String tilStreng(int[] tabell) {
-		String tekst= "[";
-		for (int i=0; i<tabell.length; i++) {
-			tekst=tekst + tabell[i];
-			
-			if (i!=tabell.length-1) {
-				tekst=tekst + ",";
+		String tekst = "[";
+		for (int i = 0; i < tabell.length; i++) {
+			tekst = tekst + tabell[i];
+
+			if (i != tabell.length - 1) {
+				tekst = tekst + ",";
 			}
 		}
-		tekst=tekst + "]";
+		tekst = tekst + "]";
 		return tekst;
-		
-		
+
 	}
 
 	// c) Sivert
 	public static int summer(int[] tabell) {
 		int sum = 0;
-		for (int i: tabell) {
+		for (int i : tabell) {
 			sum += i;
 		}
 		return sum;
-		
+
 //		int i = 0;
 //		while (i<tabell.length) {
 //			sum += tabell[i];
 //			i++;
 //		}
 //		return sum;
-		
+
 //		for (int i=0; i<tabell.length; i++) {
 //		sum += tabell[i];
 //	} 
 //	return sum;
 
 	}
-		
+
 	// d) Anders
 	public static boolean finnesTall(int[] tabell, int tall) {
 
@@ -77,19 +76,18 @@ public class Tabeller {
 
 	}
 
-
 	// e) Fredrik
 	public static int posisjonTall(int[] tabell, int tall) {
 
-		boolean exists =false;
-		int i=0;
-		while(!exists && i<tabell.length) {
-			if (tabell[i]==tall) {
-				exists=true;
-				
+		boolean exists = false;
+		int i = 0;
+		while (!exists && i < tabell.length) {
+			if (tabell[i] == tall) {
+				exists = true;
+
 				return i;
-			}
-			else i++;
+			} else
+				i++;
 		}
 		return -1;
 
@@ -97,21 +95,31 @@ public class Tabeller {
 
 	// f) Sivert
 	public static int[] reverser(int[] tabell) {
-		int [] nyTabell = new int [tabell.length];
-		for (int i=0; i<tabell.length/2; i++) {
+		int[] nyTabell = new int[tabell.length];
+		for (int i = 0; i < tabell.length / 2; i++) {
 			int reverse = tabell[i];
-			tabell[i]=tabell[tabell.length-i-1];
-			tabell[tabell.length-i-1]=reverse;
-			
+			tabell[i] = tabell[tabell.length - i - 1];
+			tabell[tabell.length - i - 1] = reverse;
+
 		}
 		return tabell;
 	}
 
-	// g) Anders
+	// g) Anders, som kan avgjere om ein array (tabell) av heiltal er sortert
+	// stigande
 	public static boolean erSortert(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("erSortert ikke implementert");
+		boolean rekkefolge = true;
+
+		int i = 1;
+		while (rekkefolge && i < tabell.length) {
+			if (tabell[i - 1] <= tabell[i])
+				i++;
+			else
+				rekkefolge = false;
+		}
+		return rekkefolge;
+
 	}
 
 	// h) Fredrik
@@ -119,11 +127,11 @@ public class Tabeller {
 
 		int[] newTable = new int[tabell1.length + tabell2.length];
 		int i;
-		for (i=0; i<tabell1.length; i++) {
+		for (i = 0; i < tabell1.length; i++) {
 			newTable[i] = tabell1[i];
 		}
-		for (int j=0; j<tabell2.length; j++) {
-			newTable[i]=tabell2[j];
+		for (int j = 0; j < tabell2.length; j++) {
+			newTable[i] = tabell2[j];
 			i++;
 		}
 		return newTable;
